bc5d22c836fae52a2d9925e16914666ca5502e43
emsqr <razmhz11@gmail.com>
2026-01-07 13:39:57 +0545

    Used crossterm key event

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,5 @@
+use crossterm::event::{Event::Key, KeyCode::Char, read};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{self, Read};
 
 pub struct Editor {}
 
@@ -10,24 +10,21 @@ impl Editor {
 
     pub fn run(&self) {
         enable_raw_mode().unwrap();
-        for b in io::stdin().bytes() {
-            let b = match b {
-                Ok(b) => b,
-                Err(err) => {
-                    println!("Error: {}", err);
-                    continue;
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{event:?} \r");
+
+                    if let Char(c) = event.code {
+                        if c == 'q' {
+                            break;
+                        }
+                    }
                 }
-            };
-            let c = b as char;
-            if c.is_control() {
-                println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-            } else {
-                println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-            }
-            if c == 'q' {
-                disable_raw_mode().unwrap();
-                break;
+                Err(err) => println!("Error: {err}"),
+                _ => (),
             }
         }
+        disable_raw_mode().unwrap();
     }
 }
