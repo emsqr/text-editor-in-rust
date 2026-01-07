ca73acebd132c4ca2d6d5260a0ef3b548c2bb28f
emsqr <razmhz11@gmail.com>
2026-01-07 14:19:34 +0545

    Improved error handling

--- src/editor.rs
+++ src/editor.rs
@@ -9,22 +9,26 @@ impl Editor {
     }
 
     pub fn run(&self) {
-        enable_raw_mode().unwrap();
-        loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{event:?} \r");
+        if let Err(err) = self.repl() {
+            panic!("{err:#?}");
+        }
+        println!("Goodbye.\r\n");
+    }
 
-                    if let Char(c) = event.code {
-                        if c == 'q' {
-                            break;
-                        }
+    fn repl(&self) -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+        loop {
+            if let Key(event) = read()? {
+                println!("{event:?} \r");
+
+                if let Char(c) = event.code {
+                    if c == 'q' {
+                        break;
                     }
                 }
-                Err(err) => println!("Error: {err}"),
-                _ => (),
             }
         }
-        disable_raw_mode().unwrap();
+        disable_raw_mode()?;
+        Ok(())
     }
 }
